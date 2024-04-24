12d8f83b3201f95eb585ffea611050b539504348
rawsodium <deannat2003@gmail.com>
2024-04-23 20:59:12 -0400

    fix distortion, correct implementation of ping-pong

--- render.cpp
+++ render.cpp
@@ -13,6 +13,7 @@ http://bela.io
 #include <vector>
 #include <cmath>
 #include <libraries/Biquad/Biquad.h>
+
 #include "MonoFilePlayer.h"
 
 // Name of the sound file (in project folder)
@@ -27,27 +28,10 @@ std::vector<float> delayBufferRightChannel;
 
 unsigned int writePointerLeft = 0;
 unsigned int writePointerRight = 0;
-unsigned int readPointerLeft = 0;
-unsigned int readPointerRight = 0;
+float readPointerLeft = 0.0;
+float readPointerRight = 0.0;
 
-// keep track of prev,second, and third previous delay trimmer vals
-float prevDelayL = 0.0;
-float prevDelayR = 0.0;
-float secondPrevDelayL = 0.0;
-float secondPrevDelayR = 0.0;
-float thirdPrevDelayL = 0.0;
-float thirdPrevDelayR = 0.0;
-
-// keep track of prev delay in samp vals
-float prevDelayInSampsL = 0.0;
-float prevDelayInSampsR = 0.0;
-
-float delayLeft = 0.0;
-float delayRight = 0.0;
-float feedback = 0.0;
-float wetFactor = 0.0;
-float volume = 1.0;
-
+// for interpolation of both channels
 int indexAboveLeft = 0;
 int indexAboveRight = 0;
 int indexBelowLeft = 0;
@@ -57,6 +41,7 @@ float fractionAboveRight = 0.0;
 float fractionBelowLeft = 0.0;
 float fractionBelowRight = 0.0;
 
+// declare variable for input up here so it is initialized
 float in = 0.0;
 
 // store current and last button states
@@ -64,12 +49,10 @@ int currentButtonStatus = HIGH;
 int lastButtonStatus = HIGH;
 
 // button pin
-const int kButtonPin = 6;
+const int kButtonPin = 0;
 
-// use lp filter on L and R channels to cut noise... hopefully
-Biquad lpLeft;
-Biquad lpRight;
-
+// lowpass filter
+Biquad lowPass;
 
 bool setup(BelaContext *context, void *userData)
 {
@@ -88,90 +71,68 @@ bool setup(BelaContext *context, void *userData)
     delayBufferLeftChannel.resize(0.75 * context->audioSampleRate);
     delayBufferRightChannel.resize(0.75 * context->audioSampleRate);
 
-    // settings for filters
-    Biquad::Settings settings{ .fs = context->audioSampleRate, .type = Biquad::lowpass};
-
-    // setup filter with proper parameters
-    lpLeft.setup(settings);
-    lpLeft.setQ(0.71);
-    lpLeft.setPeakGain(1.0);
-
-    lpRight.setup(settings);
-    lpRight.setQ(0.71);
-    lpRight.setPeakGain(1.0);
-
     // set initial delay to 0.1 seconds on each side, for now.
     readPointerLeft = (writePointerLeft - (int)(0.1 * context->audioSampleRate) + delayBufferLeftChannel.size()) % delayBufferLeftChannel.size();
     readPointerRight = (writePointerRight - (int)(0.1 * context->audioSampleRate) + delayBufferRightChannel.size()) % delayBufferRightChannel.size();
+
+    // settings for LPF
+    Biquad::Settings settings{ .fs = context->audioSampleRate, .type = Biquad::lowpass};
+
+    lowPass.setup(settings);
+    lowPass.setQ(0.707);
+    lowPass.setPeakGain(1.0);
+
+    lowPass.setFc(10.0);
 
     return true;
 }
 
 void render(BelaContext *context, void *userData)
 {
-    // set center frequency of each notch filter
-    lpLeft.setFc(820);
-    lpRight.setFc(820);
-
-    // average the current and last three delay values read from the trimmers, to reduce jittering
-    float averagedDelayLeft = (delayLeft + prevDelayL + secondPrevDelayL + thirdPrevDelayL) / 4.0;
-    float averagedDelayRight = (delayRight + prevDelayR + secondPrevDelayR + thirdPrevDelayR) / 4.0;
-
-    int delayInSampsLeft = averagedDelayLeft * context->audioSampleRate;
-    int delayInSampsRight = averagedDelayRight * context->audioSampleRate;
-
-    int avgDISL = (delayInSampsLeft + prevDelayInSampsL) / 2;
-    int avgDISR = (delayInSampsRight + prevDelayInSampsR) / 2;
-
-    // use modulo to browse circular buffers
-    readPointerLeft = (writePointerLeft - avgDISL  + delayBufferLeftChannel.size()) % delayBufferLeftChannel.size();
-    readPointerRight = (writePointerRight - avgDISR  + delayBufferRightChannel.size()) % delayBufferRightChannel.size();
-
     for(unsigned int n = 0; n < context->audioFrames; n++) {
         // check for a button press
         currentButtonStatus = digitalRead(context, n, kButtonPin);
 
-        // read delay times from trimmers (L in input0, R in input1)
+        // read delay time from trimmer
         float input0 = analogRead(context, n/2, 0); // read in analog 0
-        float input1 = analogRead(context, n/2, 1); // read in analog 1
+        input0 = lowPass.process(input0);
+
         // read feedback from trimmer
-        float input2 = analogRead(context, n/2, 2); // read in analog 2
+        float input1 = analogRead(context, n/2, 1); // read in analog 1
         // read wet/dry mix from trimmer
-        float input3 = analogRead(context, n/2, 3); // read in analog 3
+        float input2 = analogRead(context, n/2, 2); // read in analog 2
         // volume control
+        float input3 = analogRead(context, n/2, 3); // read in analog 3
+        // wet gain control
         float input4 = analogRead(context, n/2, 4); // read in analog 4
 
-        // map delay times to normal ranges - 1ms to 500ms
-        delayLeft = map(input0, 0, 3.3 / 4.096, 0.01, 0.5);
-        delayRight = map(input1, 0, 3.3 / 4.096, 0.01, 0.5);
+        // map delay time to normal range - 10ms to 500ms
+        float delayTime = map(input0, 0, 3.3 / 4.096, 0.01, 0.5);
 
-        thirdPrevDelayL = secondPrevDelayL;
-        secondPrevDelayL = prevDelayL;
-        prevDelayL = delayLeft;
-        thirdPrevDelayR = secondPrevDelayR;
-        secondPrevDelayR = prevDelayR;
-        prevDelayR = delayRight;
-
-        prevDelayInSampsL = delayInSampsLeft;
-        prevDelayInSampsR = delayInSampsRight;
-
-        // map feedback and mix values to normal ranges (0 to 1)
-        feedback = map(input2, 0, 3.3 / 4.096, 0, 0.75);
-        wetFactor = map(input3, 0, 3.3 / 4.096, 0, 1.0);
-
-        // map volume
-        volume = map(input4, 0, 3.3 / 4.096, 0, 1);
+        // map feedback, mix, volume, and wet gain
+        float feedback = map(input1, 0, 3.3 / 4.096, 0, 0.75);
+        float wetFactor = map(input2, 0, 3.3 / 4.096, 0, 1.0);
+        float volume = map(input3, 0, 3.3 / 4.096, 0, 1);
+        float gain_wet = map(input4, 0, 3.3 / 4.096, 0.5, 1.0);
 
         float dryFactor = 1.0f - wetFactor;
 
-        // process input sample
         // change input based on button press
-        if (currentButtonStatus == LOW && lastButtonStatus == HIGH) {
+        // default: direct audio input, button press = pre-loaded sound file
+        if (currentButtonStatus == LOW) {
             in = gPlayer.process();
         } else {
-            in = audioRead(context, n/2, 0);
+            in = audioRead(context, n, 0);
         }
+
+        // reset button status
         lastButtonStatus = currentButtonStatus;
+
+        float delayInSampsLeft = delayTime * context->audioSampleRate;
+        float delayInSampsRight = delayTime * context->audioSampleRate;
+
+        readPointerLeft = fmodf( (float)writePointerLeft - (float)delayInSampsLeft + (float)delayBufferLeftChannel.size(), (float)delayBufferLeftChannel.size() );
+        readPointerRight = fmodf( (float)writePointerRight - (float) delayInSampsRight + (float)delayBufferRightChannel.size(), (float) delayBufferRightChannel.size());
 
         // read at fractional place in delay buffers
         indexBelowLeft = floorf(readPointerLeft);
@@ -181,7 +142,7 @@ void render(BelaContext *context, void *userData)
         fractionAboveLeft = readPointerLeft - indexBelowLeft;
         fractionBelowLeft = 1.0 - fractionAboveLeft;
 
-        float delayBufLeft = fractionBelowLeft * delayBufferLeftChannel[indexBelowLeft] + fractionAboveLeft * delayBufferLeftChannel[indexAboveLeft];
+        float delayedOutputLeft = fractionBelowLeft * delayBufferLeftChannel[indexBelowLeft] + fractionAboveLeft * delayBufferLeftChannel[indexAboveLeft];
 
         indexBelowRight = floorf(readPointerRight);
         indexAboveRight = indexBelowRight + 1;
@@ -190,15 +151,15 @@ void render(BelaContext *context, void *userData)
         fractionAboveRight = readPointerRight - indexBelowRight;
         fractionBelowRight = 1.0 - fractionAboveRight;
 
-        float delayBufRight = fractionBelowRight * delayBufferRightChannel[indexBelowRight] + fractionAboveRight * delayBufferRightChannel[indexAboveRight];
+        float delayedOutputRight = fractionBelowRight * delayBufferRightChannel[indexBelowRight] + fractionAboveRight * delayBufferRightChannel[indexAboveRight];
 
-        // Read the output from the buffer, at the location expressed by the offset
-        float outLeft = in * dryFactor + delayBufRight * wetFactor;
-        float outRight = in * dryFactor + delayBufLeft * wetFactor;
+
+        float delayedInputLeft = in + delayedOutputRight * feedback;
+        float delayedInputRight = delayedOutputLeft * feedback;
 
         // write input and feedback to buffers
-        delayBufferLeftChannel[writePointerLeft] = in + feedback * outLeft;
-        delayBufferRightChannel[writePointerRight] = in + feedback * outRight;
+        delayBufferLeftChannel[writePointerLeft] = delayedInputLeft;
+        delayBufferRightChannel[writePointerRight] = delayedInputRight;
 
         // increment pointers
         writePointerLeft++;
@@ -217,13 +178,13 @@ void render(BelaContext *context, void *userData)
             readPointerRight = 0;
         }
 
-
-        outLeft = lpLeft.process(outLeft);
-        outRight = lpRight.process(outRight);
+        // Read the output from the buffer, at the location expressed by the offset
+        float outLeft = in * dryFactor + delayedOutputLeft * wetFactor;
+        float outRight = in * dryFactor + delayedOutputRight * wetFactor;
 
         // attenuate output for right now while we cut distortion
         outLeft *= volume;
-        outRight *= volume;
+        outRight *= (volume * gain_wet);
 
         // Write the L and R different channels
         audioWrite(context, n, 0, outLeft);
